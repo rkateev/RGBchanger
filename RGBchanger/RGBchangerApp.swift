3bc47d22e618c084d74570b9ec144171d88c6483
rkateev <60319058+rkateev@users.noreply.github.com>
2021-11-01 01:55:44 +0300

    Initial Commit

---
+++ RGBchanger/RGBchangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBchangerApp.swift
+//  RGBchanger
+//
+//  Created by  Husein on 01.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct RGBchangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
